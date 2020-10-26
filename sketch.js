74ce433bc5496954f51d2865132e94193b80131c
Filippo-Tortelli <filippo.tortelli@gmail.com>
2020-10-26 17:41:25 +0100

    versione finale

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ let sopraccigliaImage;
 let mySong;
 let analyzer;
 
+let myPersonalWords = [];
+
 function preload(){
   pavaImage = loadImage("./assets/images/pava.png");
   boccaImage = loadImage("./assets/images/bocca.png");
@@ -17,7 +19,6 @@ function setup() {
   analyzer = new p5.Amplitude();
   analyzer.setInput(mySong);
 
-  //mySong.play();
 }
 
 function draw() {
@@ -33,14 +34,85 @@ function draw() {
   image(boccaImage, width/2,(height/2+volume), boccaImage.width, boccaImage.height);
   image(sopraccigliaImage, width/2,(height/2-(volume/2)), sopraccigliaImage.width, sopraccigliaImage.height);
 
-  console.log(volume);
+  if (mySong.isPlaying() == false) {
+    var textPlay = "Click to play";
+    textFont("Crimson Pro");
+    textAlign(CENTER);
+    textSize(120);
+    fill(255);
+    text(textPlay, width/2, height/2);
+  }
+
+  for(let i = 0; i < myPersonalWords.length; i++) {
+    myPersonalWords[i].run();
+  }
 }
 
 function mouseClicked() {
     if (mySong.isPlaying() == false) {
       mySong.play();
+      addWord();
+      addWord();
+      addWord();
+      addWord();
+      addWord();
+      addWord();
     }
-    else {
-      mySong.stop();
+}
+
+function addWord() {
+  let possibleDirection = [-3,3];
+  const orizzontalDirection = random(possibleDirection);
+  const verticalDirection = random(possibleDirection);
+  const aNewWord = new Word(random(0,width), random(0,height), orizzontalDirection, verticalDirection);
+  myPersonalWords.push(aNewWord);
+}
+
+class Word {
+  constructor(temp_x, temp_y, temp_do, temp_dv) {
+    this.x = temp_x;
+    this.y = temp_y;
+    this.do = temp_do;
+    this.dv = temp_dv;
+  }
+
+  updatePosition() {
+    if (this.x > (windowWidth-20)) {
+        this.do = (this.do)*(-1);
     }
+
+    if (this.x < (20)) {
+        this.do = (this.do)*(-1);
+    }
+
+    this.x += this.do;
+
+    if (this.y > (windowHeight-20)) {
+        this.dv = (this.dv)*(-1);
+    }
+
+    if (this.y < (20)) {
+        this.dv = (this.dv)*(-1);
+    }
+
+    this.y += this.dv;
+
+  }
+
+  display() {
+    push();
+    var textWord = "Grande Bocelli!";
+    textFont("Crimson Pro");
+    textAlign(CENTER);
+    textSize(30);
+    fill(255);
+    text(textWord, this.x, this.y);
+    pop();
+
+  }
+
+  run() {
+    this.updatePosition();
+    this.display();
+  }
 }
